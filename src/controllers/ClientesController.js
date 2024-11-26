1a3323ea49113cca61f9400a6aba47ad2efaacee
Arthur Manenti <arthur.manenti.videira@gmail.com>
2024-11-26 15:34:46 -0300

    CRUD Clientes completo

--- src/controllers/ClientesController.js
+++ src/controllers/ClientesController.js
@@ -1,5 +1,5 @@
-const { Sequelize } = require('sequelize');
-const {Cliente} = require('../models')
+const { Sequelize, where } = require('sequelize');
+const { Cliente } = require('../models')
 
 
 class ClienteController {
@@ -11,6 +11,73 @@ class ClienteController {
             return res.status(500).send(erro)
         }
     }
+
+    static async pegaClientePorId(req, res) {
+        try {
+            const dados = await Cliente.findByPk(req.params.id);
+            if (dados === null) {
+                return res.status(404).json({ mensagem: "Cliente não encontrado" });
+            }
+            return res.status(200).json(dados);
+        } catch (erro) {
+            return res.status(500).send(erro);
+        }
+    }
+
+    static async novoCliente(req, res) {
+        try {
+            const { nome, email, senha } = req.body;
+            const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+
+            if (!nome || !email || !senha) {
+                return res.status(400).json({ mensagem: 'Campos obrigatórios faltando: nome, email, telefone, endereco.' });
+            } else if (!emailRegex.test(email)) {
+                return res.status(400).json({ mensagem: 'Email inválido.' });
+            }
+
+            const clienteNovo = await Cliente.create(req.body);
+            return res.status(201).json({ mensagem: `Novo cliente cadastrado de ID: ${clienteNovo.id}.` });
+
+        } catch (erro) {
+            console.error(erro);
+            return res.status(500).send({ mensagem: 'Erro interno do servidor.' });
+        }
+    }
+
+    static async alterarCliente(req, res) {
+        try {
+            const { nome, email, senha } = req.body;
+            const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+
+            if (!nome || !email || !senha) {
+                return res.status(400).json({ mensagem: 'Campos obrigatórios faltando: nome, email, telefone, endereco.' });
+            } else if (!emailRegex.test(email)) {
+                return res.status(400).json({ mensagem: 'Email inválido.' });
+            }
+
+            const cliente = await Cliente.findByPk(req.params.id);
+            await cliente.update(req.body)
+            res.status(200).json({ message: "Cliente modificado com sucesso!" })
+
+        } catch (erro) {
+            return res.status(500).send(erro)
+        }
+    }
+
+    static async excluirCliente(req, res) {
+        try {
+            const id = req.params.id
+            await Cliente.destroy({
+                where: {
+                    id: id
+                }
+            })
+            return res.status(200).json({ mensagem: `Usuário de ID: ${id} foi deletado com sucesso!` })
+        } catch (erro) {
+            return res.status(500).send(erro)
+        }
+    }
 }
+
 
 module.exports = ClienteController
--- src/routes/clientesRoute.js
+++ src/routes/clientesRoute.js
@@ -4,5 +4,9 @@ const ClienteController = require('../controllers/ClientesController.js')
 const router = Router()
 
 router.get('/clientes', (req, res) => ClienteController.pegaTudo(req, res))
+router.get('/clientes/:id', (req, res) => ClienteController.pegaClientePorId(req, res))
+router.post('/clientes', (req, res) => ClienteController.novoCliente(req, res))
+router.put('/clientes/:id', (req, res) => ClienteController.alterarCliente(req, res))
+router.delete('/clientes/:id', (req, res) => ClienteController.excluirCliente(req, res))
 
 module.exports = router
